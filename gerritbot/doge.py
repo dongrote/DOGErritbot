f5e4260b5f47cd9c28da1fe5440044b217336272
Don Grote <dgrote@tacnetsol.com>
2014-02-13 10:43:43 -0500

    Removed canned doge content

--- gerritbot/doge.py
+++ gerritbot/doge.py
@@ -1,41 +1,6 @@
 import random
 
 
-doge_prefixes = [
-    '',
-    'SO ',
-    'VERY ',
-    'MUCH ',
-    'WOW ',
-    'VERY MUCH ',
-    'SO MUCH ',
-]
-doge_positive = [
-    'CODE',
-    'BRILLIANCE',
-    'OBJECTS',
-    'SUCCESS',
-    'WIN',
-    'SUNSHINE',
-    'GREATNESS',
-]
-doge_neutral = [
-    'COMMENT',
-    'DISCUSSION',
-]
-doge_negative = [
-    'GARBAGE',
-    'TRASH',
-    'POOP',
-    'FAIL',
-    'FAILURE',
-    'FAILBOAT',
-    'CLOUDS',
-    'DEMOTION',
-    'REJECTION',
-]
-
-
 class DogeMessage(object):
     def __init__(self, prefixes=[], positive=[], neutral=[], negative=[]):
         self.prefixes = prefixes
